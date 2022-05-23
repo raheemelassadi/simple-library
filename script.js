5c69f69e414938bbd116d3f29f79acc8441d8695
raheemelassadi <raheemelassadi1@gmail.com>
2022-05-23 14:36:17 -0700

    renamed a vouple variables

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ const form = document.querySelector('.form');
 const submitBtn = document.querySelector('#submit');
 const container = document.getElementsByClassName('container');
 const getAuthor = document.getElementById('author');
-const getNumOfPages = document.getElementById('numOfPages');
+const pages = document.getElementById('numOfPages');
 const getRead = document.getElementById('read');
 const getTitle = document.getElementById('title');
-let newBook = new book(getTitle.value, getAuthor.value, getNumOfPages.value, getRead.value);
 
 
 
 // constructor for making book
-function book(title, author, numOfPages, read){
+function book(title, author, pages, read){
     this.title = title;
     this.author = author;
-    this.numOfPages = numOfPages;
+    this.pages = pages;
     this.read = read;
 }
 
@@ -30,7 +29,7 @@ addBtn.addEventListener('click', function(){
     form.style.display = 'inline-block';
     title.value = "";
     author.value = "";
-    numOfPages.value = "";
+    pages.value = "";
     read.value = "read";
 })
 
@@ -40,37 +39,11 @@ submitBtn.addEventListener('click', function(newBook){
     form.style.display = 'none';
     newBook.title = getTitle.value;
     newBook.author = getAuthor.value;
-    newBook.numOfPages = getNumOfPages.value;
+    newBook.pages = pages.value;
     newBook.read = getRead.value;
 
 
     let newElem = document.createElement('li');
-    newElem.innerHTML = `Title: ${newBook.title} </br> Author: ${newBook.author} </br> Pages: ${newBook.numOfPages} </br> Read: ${newBook.read}`
+    newElem.innerHTML = `Title: ${newBook.title} </br> Author: ${newBook.author} </br> Pages: ${newBook.pages} </br> Read: ${newBook.read}`
     list.appendChild(newElem);
 })
-
-
-
-
-
-
-// dummy books
-const bookOne = new book('harry potter');
-bookOne.author = "jk rowling";
-bookOne.numOfPages = '400';
-bookOne.read = 'not read';
-const bookTwo = new book('the notebook');
-bookTwo.author = "Nicholas Sparks";
-bookTwo.numOfPages = '553';
-bookTwo.read = 'Read';
-
-storeBooks(bookOne);
-storeBooks(bookTwo);
-
-for (const book of myLibrary){
-    let elem = document.createElement('li');
-    elem.innerHTML = `Title: ${book.title} </br> Author: ${book.author} </br> Pages: ${book.numOfPages} </br> Read: ${book.read}`
-    list.appendChild(elem);
-}
-
-
